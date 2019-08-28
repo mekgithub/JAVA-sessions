ba4d1c8d603d677c5476faea814e365f836e3648
mekgithub <54605592+mekgithub@users.noreply.github.com>
2019-08-27 19:44:39 -0500

    Add files via upload

---
+++ Bicycle.java
@@ -0,0 +1,12 @@
+package abstractionandinterface;
+
+abstract class Bicycle {
+    abstract void speedUp();
+    abstract void slowDown();
+    void start(){
+        System.out.println("Started to pedal");
+    }
+    void stop(){
+        System.out.println("Bike stopped");
+    }
+}
